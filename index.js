c8a79ac61a4da8c1a30843cbe139048b106cf00e
Joe <joezdru@gmail.com>
2021-05-09 15:44:50 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+
+}
+
+function subtract() {
+ 
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a = 952, b = 1) {
+    return a + b
+}
+
+function subtract(a = 387, b = 1) {
+    return a - b
+}
+
+function multiply(a = 14328, b = 1) {
+    return a * b
+}
+
+function divide(a = 2, b = 1) {
+    return a / b
+}
+
+let n = 1.323
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(n) {
+   return parseInt(n , 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
